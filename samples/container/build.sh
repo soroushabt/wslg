0b16e8c442223aa111d6c889e7bfb949b1b5ee9e
soroush_linux <m.soroush.abotaleb@gmail.com>
2023-02-09 15:09:34 +0330

    rideman

--- samples/container/build.sh
+++ samples/container/build.sh
@@ -3,3 +3,5 @@
 sudo docker build -t xclock -f Dockerfile.xclock .
 sudo docker build -t glxinfo -f Dockerfile.glxinfo .
 sudo docker build -t glxgears -f Dockerfile.glxgears .
+
+teststasetasetasdf
